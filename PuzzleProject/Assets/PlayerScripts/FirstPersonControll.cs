f290c51669927d0e13bac0a6a0f697570c684c6c
Callmejayden <jayden.overdahl@gmail.com>
2023-02-22 17:16:36 -0600

    Created IInteract Script and Layer
    Having movement error with HandleMouseLook:
    
    NullReferenceException: Object reference not set to an instance of an object
    FirstPersonControll.HandleMouseLook () (at Assets/PlayerScripts/FirstPersonControll.cs:141)
    FirstPersonControll.Update () (at Assets/PlayerScripts/FirstPersonControll.cs:111)

--- PuzzleProject/Assets/PlayerScripts/FirstPersonControll.cs
+++ PuzzleProject/Assets/PlayerScripts/FirstPersonControll.cs
@@ -9,11 +9,19 @@ public class FirstPersonControll : MonoBehaviour
 {
     public bool CanMove { get; private set; } = true;
 
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canInteract = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode InteractKey = KeyCode.Mouse0;
+
     // Speed and gravity
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
     [SerializeField] private float gravity = 30.0f;
     
+
     // Look speed of the x and y and the limits of how far you can look
     [Header("Look Parameters")]
     [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
@@ -21,7 +29,14 @@ public class FirstPersonControll : MonoBehaviour
     [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
 
-    // varibles
+    //Setting Interaction Distance, Layer, etc
+    [Header("Interaction")]
+    [SerializeField] private Vector3 interactionRayPoint = default;
+    [SerializeField] private float interactionDistance = default;
+    [SerializeField] private LayerMask interactionLayer = default;
+    private IInteract currentInteractable;
+
+    // variables
     private Camera playerCamera;
     private CharacterController characterController;
 
@@ -30,6 +45,52 @@ public class FirstPersonControll : MonoBehaviour
 
     private float rotationX = 0;
 
+    //Methods
+
+    //Check if is looking
+    private void HandleInteractionCheck()
+    {
+        if (Physics.Raycast(playerCamera.ViewportPointToRay(interactionRayPoint), out RaycastHit hit, interactionDistance))
+        {
+            //8:00 in video add new layer top right inspector : "IInteract" at number 9
+            //if on layer 9                      AND dont have current interactable OR if the object I am looking at is not equal to the current interactable
+            if (hit.collider.gameObject.layer == 9 && (currentInteractable == null || hit.collider.gameObject.GetInstanceID() != currentInteractable.GetInstanceID()))
+            {
+                hit.collider.TryGetComponent<IInteract>(out currentInteractable);
+
+                if (currentInteractable)
+                    currentInteractable.OnFocus();
+
+            }
+        }
+
+        //When there is no interactable 
+        else if (currentInteractable)
+        {
+            currentInteractable.OnLoseFocus();
+            currentInteractable = null;
+        }
+    }
+
+
+    //When Pressing Interact Button
+    private void HandleInteractionInput()
+    {
+        // if interact key is pressed  and  there is something to interact with  and  Create Ray at center of screen with interaction distance  and  is on the interact Layer
+        if (Input.GetKeyDown(InteractKey) && currentInteractable != null && Physics.Raycast(playerCamera.ViewportPointToRay(interactionRayPoint), out RaycastHit hit, interactionDistance, interactionLayer))
+        {
+            currentInteractable.OnInteract();
+
+
+
+
+        }
+
+
+
+        //12:30 for application
+
+    }
 
     void Awake()
     {
@@ -50,6 +111,14 @@ public class FirstPersonControll : MonoBehaviour
             HandleMouseLook();
 
             ApplyFinalMovements();
+
+            if (canInteract)
+            {
+                //look for interactable objects
+                HandleInteractionCheck();
+                //when hits the interact key
+                HandleInteractionInput();
+            }
         }
     }
 
