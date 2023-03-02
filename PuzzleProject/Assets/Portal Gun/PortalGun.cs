61c294f7b62323906035042e884398c98af1f006
Arthur <adamc52102@gmail.com>
2023-03-02 11:25:54 -0600

    script to shoot portals
    this is a basic script that move portal's from point A to B with the click of a button not fully bugged out yet but allows for testing

---
+++ PuzzleProject/Assets/Portal Gun/PortalGun.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGun : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject A;
+    public GameObject B;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootA();
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ShootB();
+        }
+    }
+    public void ShootA()
+    {
+        RaycastHit rayHit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out rayHit))
+        {
+            Vector3 hitPos = rayHit.point;
+
+            A.transform.position = hitPos;
+        }
+    }
+
+    public void ShootB()
+    {
+        RaycastHit rayHit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out rayHit))
+        {
+            Vector3 hitPos = rayHit.point;
+
+            B.transform.position = hitPos;
+        }
+
+    }
+}
